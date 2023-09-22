b3232aabada8cd34a8100181a78f8399f1dbaac4
Lazaro Camero <94584715+Laz12@users.noreply.github.com>
2023-09-22 02:29:47 -0400

    Update Cart_Service.py

--- Cart_Service.py
+++ Cart_Service.py
@@ -1,76 +1,98 @@
 from flask import Flask, jsonify, request
+import requests
 
 app = Flask(__name__)
 
-cart_contents = [
-    {'user_id': 1, 'product_id': 4,'name': 'Apple', 'quantity': 2, 'price_per_item': 10.0},
-    {'user_id': 2, 'product_id': 5, 'quantity': 1, 'price_per_item': 20.0}
-]
+# URL of the Product Service
+PRODUCT_SERVICE_URL = 'http://localhost:5000'  # Replace with the actual URL
 
+# Sample user cart data (you can use a database in a real application)
+user_carts = {
+    1: {1: 3},
+}
 
 # Endpoint 1: Retrieve current contents of a user’s shopping cart, including product names, quantities, and total prices.
 @app.route('/cart/<int:user_id>', methods=['GET'])
-def get_content():
-    return jsonify({"cart content": cart_contents})
+def get_cart(user_id):
+    user_cart = user_carts.get(user_id, {})
+    cart_contents = []
 
+    for product_id, quantity in user_cart.items():
+        product_info = get_product_info(product_id)
+        if product_info:
+            cart_contents.append({
+                "product_name": product_info['name'],
+                "quantity": quantity,
+                "total_price": product_info['price'] * quantity
+            })
+
+    return jsonify(cart_contents)
 
 # Endpoint 2: Add a specified quantity of a product to the user’s cart
 @app.route('/cart/<int:user_id>/add/<int:product_id>', methods=['POST'])
-def new_product(user_id, product_id):
-    # Parse the request data to get the quantity
-    data = request.get_json()
-    quantity = data.get('quantity', 1)  # Default to 1 if not specified
-    cart_data = {
-        'user_id': user_id,
-        'cart': {
-            product_id: {
-                'product_name': 'Apple',  # Replace with actual product details
-                'quantity': quantity
-            }
-        }
-    }
-    # cart_contents[item] = cart_contents.get(item, 0) + quantity
-    return jsonify({'cart_contents': cart_contents})
- 
+def add_to_cart(user_id, product_id):
+    quantity = request.get_json().get('quantity', 1)
+
+    product_info = get_product_info(product_id)
+    if not product_info:
+        return jsonify({"error": "Product not found"}), 404
+
+    user_cart = user_carts.setdefault(user_id, {})
+    user_cart[product_id] = user_cart.get(product_id, 0) + quantity
+    
+    return jsonify({"message": f"{quantity} {product_info['name']} added to cart"}), 201
 
 # Endpoint 3: Remove a specified quantity of a product from the user’s cart
 @app.route('/cart/<int:user_id>/remove/<int:product_id>', methods=['POST'])
-def remove_product(user_id, product_id):
+def remove_from_cart(user_id, product_id):
+    quantity = request.get_json().get('quantity', 1)
 
-    # Parse the request data to get the quantity to remove
+    product_info = get_product_info(product_id)
+    if not product_info:
+        return jsonify({"error": "Product not found"}), 404
+
+    user_cart = user_carts.get(user_id, {})
+    if product_id in user_cart:
+        user_cart[product_id] = max(user_cart[product_id] - quantity, 0)
+        if user_cart[product_id] == 0:
+            del user_cart[product_id]
+
+    return jsonify({"message": f"{quantity} {product_info['name']} removed from cart"}), 200
+
+def get_product_info(product_id):
+    response = requests.get(f"{PRODUCT_SERVICE_URL}/products/{product_id}")
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+
+@app.route('/cart/update_product_quantity/<int:product_id>', methods=['PATCH'])
+def update_product_quantity_in_cart(product_id):
     data = request.get_json()
-    quantity_to_remove = data.get('quantity', 1)  # Default to 1 if not specified
+    quantity_change = data.get('quantity_change', 0)
 
-    # Retrieve the user's cart (assuming you have a cart data storage mechanism)
-    user_cart = next((content for content in cart_contents if content["user_id"] == user_id), None)
+    # Update the quantity in the cart locally
+    for user_id, cart in user_carts.items():
+        if product_id in cart:
+            cart[product_id] = max(cart[product_id] + quantity_change, 0)
 
-    if user_cart is None:
-        return jsonify({'error': f'User {user_id} does not have a cart'}), 404
+    # Update the quantity in the Product Service
+    update_quantity_in_product_service(product_id, quantity_change)
 
-    # Check if the product is in the user's cart
-    if product_id not in user_cart:
-        return jsonify({'error': f'Product {product_id} not found in the cart'}), 404
+    return jsonify({"message": "Product quantity updated in cart and Product Service"}), 200
 
-    # Retrieve the current quantity of the product in the cart
-    current_quantity = user_cart[product_id]['quantity']
+def update_quantity_in_product_service(product_id, quantity_change):
+    # Send a request to the Product Service to update the product quantity
+    data = {"quantity_change": quantity_change}
+    response = requests.patch(f"{PRODUCT_SERVICE_URL}/products/update_quantity/{product_id}", json=data)
 
-    # Check if the quantity to remove is valid
-    if quantity_to_remove <= 0 or quantity_to_remove > current_quantity:
-        return jsonify({'error': 'Invalid quantity to remove'}), 400
+    if response.status_code != 200:
+        return jsonify({"error": "Failed to update product quantity in Product Service"}), 500
 
-    # Update the quantity in the cart
-    user_cart[product_id]['quantity'] -= quantity_to_remove
-
-    # You may want to remove the product entry from the cart if the quantity reaches 0
-    if user_cart[product_id]['quantity'] == 0:
-        del user_cart[product_id]
-
-    # Save the updated cart back to the storage mechanism
-    save_user_cart(user_id, user_cart)  # Replace with your method to save user's cart
-
-    return jsonify({'message': f'Removed {quantity_to_remove} of product {product_id} from the cart for user {user_id}'})
-  
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
+
+
+
