46e877543ca5bd2e075d50b70b974270299c8d0f
Lazaro Camero <94584715+Laz12@users.noreply.github.com>
2023-09-22 14:32:48 -0400

    Update Cart_Service.py

--- Cart_Service.py
+++ Cart_Service.py
@@ -101,7 +101,7 @@ def update_quantity_in_product_service(product_id, quantity_change):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 
 
