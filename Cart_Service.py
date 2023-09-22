b3c63047b434b0bb8e7a227b9bbbbb0b2f8f762d
Lazaro Camero <94584715+Laz12@users.noreply.github.com>
2023-09-22 11:33:23 -0400

    Update Cart_Service.py

--- Cart_Service.py
+++ Cart_Service.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -99,7 +100,8 @@ def update_quantity_in_product_service(product_id, quantity_change):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', port=port)
 
 
 
