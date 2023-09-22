d983331e81753957c0e56c8c3ac5d3479ecd67a5
Lazaro Camero <94584715+Laz12@users.noreply.github.com>
2023-09-22 14:31:41 -0400

    Update Product_Service.py

--- Product_Service.py
+++ Product_Service.py
@@ -60,7 +60,7 @@ def update_product_quantity(product_id):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 443))
+    port = int(os.environ.get('PORT', 5000))
     host = os.environ.get('HOST', '0.0.0.0')
 
     app.run(debug=True, host=host, port=port)
